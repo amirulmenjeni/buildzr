8d2be8e64cc9629bd626b6d1ef73b35c3aa23e28
Amirul Menjeni <amirulmenjeni@gmail.com>
2024-06-18 15:11:29 +0000

    Remove old references

--- buildzr/encoders/__init__.py
+++ buildzr/encoders/__init__.py
@@ -1,4 +1,1 @@
 from .encoder import JsonEncoder
-from .software_system_encoder import SoftwareSystemEncoder
-from .workspace_encoder import WorkspaceEncoder
-from .person_encoder import PersonEncoder
