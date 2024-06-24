a3275d6ed0a5d138a73390821efeec8ab34415c4
Amirul Menjeni <amirulmenjeni@gmail.com>
2024-06-24 13:37:52 +0000

    Use proper hint typing for list

--- buildzr/models/views.py
+++ buildzr/models/views.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass, field
+from typing import List
 from .system_landscape_view import SystemLandscapeView
 from .system_context_view import SystemContextView
 from .container_view import ContainerView
@@ -7,10 +8,10 @@ from .component_view import ComponentView
 @dataclass
 class Views:
 
-    system_landscape_views: list[SystemLandscapeView] = field(default_factory=list)
+    system_landscape_views: List[SystemLandscapeView] = field(default_factory=list)
 
-    system_context_views: list[SystemContextView] = field(default_factory=list)
+    system_context_views: List[SystemContextView] = field(default_factory=list)
 
-    container_views: list[ContainerView] = field(default_factory=list)
+    container_views: List[ContainerView] = field(default_factory=list)
 
-    component_views: list[ComponentView] = field(default_factory=list)
+    component_views: List[ComponentView] = field(default_factory=list)
