ca82b68c7e9e1915ceb18d85200ce6df9cc2db9a
Amirul Menjeni <amirulmenjeni@gmail.com>
2024-06-25 23:11:57 +0000

    Convert snake case to camel case in JSON encoder

--- buildzr/encoders/encoder.py
+++ buildzr/encoders/encoder.py
@@ -1,7 +1,8 @@
 import dataclasses, json
+import humps
 
 class JsonEncoder(json.JSONEncoder):
     def default(self, obj):
         if dataclasses.is_dataclass(obj):
-            return dataclasses.asdict(obj)
+            return humps.camelize(dataclasses.asdict(obj))
         return super().default(obj)
