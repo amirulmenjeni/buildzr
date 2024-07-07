7ca768dc531ae7656bd02aa77b8273fc5d934323
Amirul Menjeni <amirulmenjeni@gmail.com>
2024-07-07 10:26:19 +0000

    Fix IDs not created in elements and relationships

--- buildzr/dsl/factory/gen_id.py
+++ buildzr/dsl/factory/gen_id.py
@@ -7,11 +7,14 @@ class GenerateId:
         1: 0,
     }
 
-    def for_workspace():
+    def for_workspace() -> int:
         GenerateId._data[0] = GenerateId._data[0] + 1
+        return GenerateId._data[0]
 
-    def for_element():
+    def for_element() -> int:
         GenerateId._data[1] = GenerateId._data[1] + 1
+        return GenerateId._data[1]
 
-    def for_relationship():
+    def for_relationship() -> int:
         GenerateId._data[1] = GenerateId._data[1] + 1
+        return GenerateId._data[1]
--- tests/test_dsl.py
+++ tests/test_dsl.py
@@ -1,5 +1,15 @@
 from buildzr.dsl import Workspace, SoftwareSystem, Person
 
+def test_element_ids():
+
+    workspace = Workspace("My Workspace", "A happy place")
+    person = Person("Super user")
+    software_system = SoftwareSystem("My Software System")
+
+    assert workspace.id is not None
+    assert person.id is not None
+    assert software_system.id is not None
+
 def test_relationship_dsl():
 
     workspace = Workspace("My Workspace", "A happy place")
