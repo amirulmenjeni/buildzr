a172d534ada005e8929f64ef47d5476190b6efe3
Amirul Menjeni <amirulmenjeni@gmail.com>
2024-08-04 13:41:08 +0000

    Fix wrong class name

--- tests/samples/implied_relationships.py
+++ tests/samples/implied_relationships.py
@@ -9,16 +9,29 @@ class ImpliedRelationships(AbstractBuilder):
 
     def build(self) -> buildzr.models.Workspace:
 
-        return Workspace("Implied Relationships")\
-               .contains(
-                   Person("User"),
-                   SoftwareSystem("Software System")\
-                   .contains(
-                       Container("Web Application")
-                   )
-                   .get(),
-               )\
-               .where(lambda u, s: [
-                    u >> "Uses" >> s.web_application,
-               ])\
-               .model
+        w = Workspace("w")\
+                .contains(
+                    Person("u"),
+                    SoftwareSystem("s")\
+                        .contains(
+                            Container("webapp")\
+                                .contains(
+                                    Component("database layer"),
+                                    Component("API layer"),
+                                    Component("UI layer"),
+                                )\
+                                .where(lambda db, api, ui: [
+                                    ui >> ("Calls HTTP API from", "http/api") >> api,
+                                    api >> ("Runs queries from", "sql/sqlite") >> db,
+                                ]),\
+                            Container("database"),
+                        )\
+                        .where(lambda webapp, database: [
+                            webapp >> "Uses" >> database
+                        ])
+                )\
+                .where(lambda u, s: [
+                    u >> "Runs SQL queries" >> s.database
+                ])
+
+        return w.model
