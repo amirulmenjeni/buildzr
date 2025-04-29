87f98645cfef6cafb14c8b812282d7fe94f9f867
Amirul Menjeni <amirulmenjeni@pm.me>
2025-04-29 11:28:33 +0000

    feat(dsl): (WIP) Update example to use context-based syntax

--- examples/system_context_and_container_view.py
+++ examples/system_context_and_container_view.py
@@ -14,50 +14,40 @@ from buildzr.dsl import (
     Group,
 )
 
-w = Workspace('w')\
-        .contains(
-            Group(
-                "My Company",
-                Person('Web Application User').labeled('u'),
-                SoftwareSystem('Corporate Web App').labeled('webapp')
-                    .contains(
-                        Container('database'),
-                        Container('api'),
-                    )\
-                    .where(lambda s: [
-                        s.api >> "Reads and writes data from/to" >> s.database,
-                    ])
-            ),
-            Group(
-                "Microsoft",
-                SoftwareSystem('Microsoft 365').labeled('email_system'),
-            )
-        )\
-        .where(lambda w: [
-            w.person().u >> [
-                desc("Reads and writes email using") >> w.software_system().email_system,
-                desc("Create work order using") >> w.software_system().webapp,
-            ],
-            w.software_system().webapp >> "sends notification using" >> w.software_system().email_system,
-        ])\
-        .with_views(
-            SystemContextView(
-                lambda w: w.software_system().webapp,
-                key='web_app_system_context_00',
-                description="Web App System Context",
-                auto_layout='lr',
-                exclude_elements=[
-                    lambda w, e: w.person().user == e,
-                ]
-            ),
-            ContainerView(
-                lambda w: w.software_system().webapp,
-                key='web_app_container_view_00',
-                auto_layout='lr',
-                description="Web App Container View",
-            )
-        )\
-        .get_workspace()
+with Workspace('w') as w:
+    with Group("My Company"):
+        u = Person('Web Application User').labeled('u')
+        webapp = SoftwareSystem('Corporate Web App').labeled('webapp')
+        with webapp:
+            database = Container('database')
+            api = Container('api')
+            api >> "Reads and writes data from/to" >> database
+    with Group("Microsoft"):
+        email_system = SoftwareSystem('Microsoft 365').labeled('email_system')
+
+    u >> [
+        desc("Reads and writes email using") >> email_system,
+        desc("Create work order using") >> webapp,
+    ]
+    webapp >> "sends notification using" >> email_system
+
+    w.with_views(
+        SystemContextView(
+            lambda w: w.software_system().webapp,
+            key='web_app_system_context_00',
+            description="Web App System Context",
+            auto_layout='lr',
+            exclude_elements=[
+                lambda w, e: w.person().u == e,
+            ]
+        ),
+        ContainerView(
+            lambda w: w.software_system().webapp,
+            key='web_app_container_view_00',
+            auto_layout='lr',
+            description="Web App Container View",
+        )
+    )
 
 # Writes the Workspace model to a JSON file.
 with open(os.path.join(os.path.curdir, f"{__file__.split('.')[0]}.json"), 'w', encoding='utf-8') as f:
